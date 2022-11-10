53b471eba64efe4aa3d7f1f433e2ff908b9c1653
sanchit-bhalla <sanchitbhalla91@gmail.com>
2022-11-10 12:29:25 +0530

    adding partials using express-ejs-layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,20 @@
 module.exports.profile = function (req, res) {
   console.log(req.url);
-  return res.end("<h1>User Profile </h1>");
+  return res.render("user_profile", {
+    title: "Profile Page",
+  });
+};
+
+// Render the Sign Up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+// Render the Sign In page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
 };
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose"); // new instance is not created. Same instance will be used which was used to setup connection with db in config folder - ?
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // It will add 2 more fields 'createdAt' and 'updatedAt' in the schema
+  }
+);
+
+// Convert Schema into Model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
